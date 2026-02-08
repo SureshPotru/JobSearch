95735f8aaab29a855692e5fbeccbd99b625283ee
SureshPotru <150933321+SureshPotru@users.noreply.github.com>
2026-02-08 18:58:17 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+LOCATION_KEYWORDS = ["hyderabad", "remote", "telangana"]
+
+MANDATORY_SKILLS = ["aws", "kubernetes", "devops"]
+
+PRIMARY_SKILLS = {
+    "aws": 25,
+    "kubernetes": 20,
+    "eks": 15,
+    "terraform": 15,
+    "ci/cd": 10,
+    "docker": 10,
+    "github actions": 10,
+    "jenkins": 10
+}
+
+SECONDARY_SKILLS = {
+    "grafana": 5,
+    "prometheus": 5,
+    "cloudwatch": 5,
+    "helm": 5,
+    "python": 5,
+    "bash": 5
+}
+
+MIN_SCORE = 70
